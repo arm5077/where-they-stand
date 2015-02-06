0a9a74f5472073380123761e7a6a932558e7805e
Andrew McGill <arm5077@gmail.com>
2015-02-05 23:57:39 -0500

    Adds angular directive and finishes styling

---
+++ app.js
@@ -0,0 +1,12 @@
+app = angular.module("opinionApp", []);
+
+app.controller("opinionController", ["$scope", "$sce", function($scope, $sce){
+	
+	$scope.issues = data.issues;
+	$scope.renderHTML = function(text){ return $sce.trustAsHtml(text); };
+	$scope.getImage = function(name){
+		//console.log(data.opinionators.map(function(datum){ if ( datum.name == name ) return datum.img }))[0];
+		return {"background-image": "url('" + data.opinionators.map(function(datum){ if ( datum.name == name ) return datum.img })[0] + "')"};
+	};
+	
+}]);
