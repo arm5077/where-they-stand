d65700256da601f54ebfd1cc5af68290703a69f7
Libbygi <isenstein.libby@gmail.com>
2015-09-22 10:23:31 -0400

    more tests

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var GoogleSpreadsheet = require('google-spreadsheet');
 var mysql = require("mysql");
 var firstBy = require('thenBy.js');
 
-//test comment
+//test commentssssss
 
 // Open mysql pool of connections
 var pool = mysql.createPool(process.env.CLEARDB_DATABASE_URL || "mysql://root@localhost/where_they_stand");
